48a54b44fefa5cbeeec2f3e0624c2d274a5816ea
Elithon Silva <elithonsilva@gmail.com>
2022-09-28 20:13:45 -0300

    valida se email informado é válido

--- app/backend/src/middlewares/validateLogin.ts
+++ app/backend/src/middlewares/validateLogin.ts
@@ -1,12 +1,18 @@
 import { Request, Response, NextFunction } from 'express';
+import ServiceLogin from '../service/ServiceLogin';
 
 const fieldsFilled = { message: 'All fields must be filled' };
+const failEmailOrPass = { message: 'Incorrect email or password' };
 
 export default class validateLogin {
   static async email(req: Request, res: Response, next: NextFunction) {
     const { email } = req.body;
 
     if (!email) return res.status(400).json(fieldsFilled);
+
+    const user = await ServiceLogin.findUser(email);
+
+    if (!user) return res.status(401).json(failEmailOrPass);
 
     next();
   }
