a17ac34d076b9c592210edc29d9ba057906eac3c
Elithon Silva <elithonsilva@gmail.com>
2022-09-28 13:44:43 -0300

    impl ModelTeam

---
+++ app/backend/src/database/models/ModelTeam.ts
@@ -0,0 +1,31 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+import Match from './ModelMatch';
+
+class Team extends Model {
+  public id: number;
+
+  public teamName: string;
+}
+
+Team.init({
+  id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  teamName: { allowNull: false, type: DataTypes.STRING },
+}, {
+  underscored: true,
+  sequelize: db,
+  timestamps: false,
+  tableName: 'teams',
+});
+
+Match.belongsTo(Team, {
+  foreignKey: 'home_team',
+  as: 'homeTeam',
+});
+
+Match.belongsTo(Team, {
+  foreignKey: 'away_team',
+  as: 'awayTeam',
+});
+
+export default Team;
