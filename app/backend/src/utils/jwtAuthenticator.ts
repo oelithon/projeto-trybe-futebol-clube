3615cc134d0b01bacf8180ee59113a04fefa4f33
Elithon Silva <elithonsilva@gmail.com>
2022-09-28 21:56:10 -0300

    impl verify token

--- app/backend/src/utils/jwtAuthenticator.ts
+++ app/backend/src/utils/jwtAuthenticator.ts
@@ -1,12 +1,20 @@
 import * as jwt from 'jsonwebtoken';
 import { IUser } from '../interfaces/InterfaceUser';
+import ServiceLogin from '../service/ServiceLogin';
 
 const secret = process.env.JWT_SECRET || 'JWT_SECRET';
 
 export default class Token {
-  static async createToken(user: IUser) {
-    const payload = jwt.sign({ user }, secret, { algorithm: 'HS256' });
+  static async createToken(email: string) {
+    const { id, role, username } = await ServiceLogin.findUser(email) as IUser;
 
-    return payload;
+    const token = jwt.sign({ id, role, username }, secret, { algorithm: 'HS256' });
+
+    return token;
+  }
+
+  static async verifyToken(token: string) {
+    const decoded = jwt.verify(token, secret);
+    return decoded;
   }
 }
