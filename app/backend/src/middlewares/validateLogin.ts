0ec582a894e2f569ac7da7c5e4cd0fdb3fb514a3
Elithon Silva <elithonsilva@gmail.com>
2022-09-28 23:23:53 -0300

    valida quando token invalido ou expirado

--- app/backend/src/middlewares/validateLogin.ts
+++ app/backend/src/middlewares/validateLogin.ts
@@ -1,10 +1,12 @@
 import { Request, Response, NextFunction } from 'express';
 import VerifyPassword from '../utils/verifyPassword';
 import ServiceLogin from '../service/ServiceLogin';
+import UserToken from '../utils/jwtAuthenticator';
 
 const fieldsFilled = { message: 'All fields must be filled' };
 const failEmailOrPass = { message: 'Incorrect email or password' };
-const notFound = { message: 'Token not found' };
+const tokenNotFound = { message: 'Token not found' };
+const tokenExpiredOrInvalid = { message: 'Token Expired or Invalid' };
 
 export default class validateLogin {
   static async email(req: Request, res: Response, next: NextFunction) {
@@ -34,7 +36,11 @@ export default class validateLogin {
   static async verifyToken(req: Request, res: Response, next: NextFunction) {
     const { authorization } = req.headers;
 
-    if (!authorization) return res.status(401).json(notFound);
+    if (!authorization) return res.status(404).json(tokenNotFound);
+
+    const token = await UserToken.verifyToken(authorization);
+
+    if (!token) return res.status(401).json(tokenExpiredOrInvalid);
 
     next();
   }
