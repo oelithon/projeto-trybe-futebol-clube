9161e14979d18d615d3c986cb6593fdbc229701a
Elithon Silva <elithonsilva@gmail.com>
2022-09-29 20:24:15 -0300

    remove importação de express sem uso

--- app/backend/src/routers/RouteLogin.ts
+++ app/backend/src/routers/RouteLogin.ts
@@ -1,5 +1,4 @@
 import * as express from 'express';
-import { Request, Response } from 'express';
 import ControllerLogin from '../controllers/ControllerLogin';
 import validateLogin from '../middlewares/validateLogin';
 
